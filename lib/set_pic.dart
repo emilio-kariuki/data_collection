b395978dcc8a1dcaaabe7c743c7543c0665ce162
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-09 14:58:00 +0300

    February 9, 2022 2:58 PM

--- lib/set_pic.dart
+++ lib/set_pic.dart
@@ -1,11 +1,13 @@
+// ignore_for_file: unnecessary_null_comparison
+
 import 'dart:io';
 
 import 'package:data_collection/get_picture.dart';
 import "package:flutter/material.dart";
 
-
 class Sett extends StatefulWidget {
-  const Sett({Key? key, required User value}) : super(key: key);
+  final User value;
+  const Sett({Key? key, required this.value}) : super(key: key);
 
   @override
   State<Sett> createState() => _SettState();
@@ -14,10 +16,12 @@ class Sett extends StatefulWidget {
 class _SettState extends State<Sett> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(title: Text("Set the Image")),
-      
+      appBar: AppBar(title: const Text("Set the Image")),
+      body: Center(
+          child: widget.value.name == null
+              ? const Text("No image selected")
+              : Image.file(widget.value.name, height: 120, width: 120)),
     );
   }
 }
