3e3830c8a721c572d39457e251dafb9a651675af
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-09 15:55:21 +0300

    February 9, 2022 3:55 PM

--- lib/test.dart
+++ lib/test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:data_collection/set_pic.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,15 +13,27 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
+  DateTime? date;
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Test Page"),
-        ),
-        body: Center(
-          child: SvgPicture.asset(widget.value.name, width: size.width * 0.4,height: size.height *0.5,),
-        ));
+      appBar: AppBar(
+        title:  Text("Test Page",),
+      ),
+      body: Center(
+          child:
+              Text(date == null ? "No date has been picked" : date.toString())),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDatePicker(
+              context: context,
+              initialDate: date ?? DateTime.now(),
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2023)
+              ).then((value) => date = value);
+        },
+      ),
+    );
   }
 }
